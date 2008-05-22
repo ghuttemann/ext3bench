3e3c11a0ce87053557d715215049d4f6d6887eb0
ghuttemann <ghuttemann@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-22 04:45:31 +0000

    Agrego la cabecera de las operaciones de fragmentación.

--- trunk/src/operaciones.h
+++ trunk/src/operaciones.h
@@ -1,7 +1,10 @@
 #ifndef OPERACIONES_H_
 #define OPERACIONES_H_
 
+/*
+ * Función que realiza las pruebas operaciones
+ * que inducen a la fragmentación de los archivos.
+ */
+void test_fragmentacion(char *path);
+
 #endif /*OPERACIONES_H_*/
-
-
-//ToDo: Cabeceras y definiciones de operaciones
