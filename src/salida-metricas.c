c7ec4a9bf10a6078aba923809427c075004278bc
ghuttemann <ghuttemann@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-22 20:21:23 +0000

    Correccion en la utilizacion de estructuras

--- src/salida-metricas.c
+++ src/salida-metricas.c
@@ -2,11 +2,11 @@
 
 
 FILE *open_csvFile(char *filename) {
-	fopen(filename,"w");
+	return abrir_archivo(filename, "w");
 }
 
-void close_csvFile(FILE fd) {
-	close(fd);
+void close_csvFile(FILE *fd) {
+	fclose(fd);
 }
 
 void print_header(FILE *csvFile, param_t *p) {
@@ -15,7 +15,7 @@ void print_header(FILE *csvFile, param_t *p) {
 	fprintf(csvFile,"# Parámetros de la Prueba\n"			);
 	fprintf(csvFile,"# -----------------------\n"			);
 	fprintf(csvFile,"pTB,pNJ,pSI,pAT\n"						);
-	fprintf(csvFile,"%d,%s,%d,%d\n",p.pTB,p.pNJ,p.pSI,p.pAT	);
+	fprintf(csvFile,"%d,%s,%d,%s\n", p->pTB, p->pNJ, p->pSI, p->pAT);
 	fprintf(csvFile,"# Resultados de las Pruebas \n"		);
 	fprintf(csvFile,"# -------------------------\n"			);
 	fprintf(csvFile,"testId,BEs,BLs,LE,LL,tCD,tBD,PF"		);
@@ -25,7 +25,7 @@ void print_header(FILE *csvFile, param_t *p) {
 	printf("# Parámetros de la Prueba\n"			);
 	printf("# -----------------------\n"			);
 	printf("|Tam. Bloque | Nivel de Journal | Sync Interval | Access time |\n"						);
-	printf("|%12d|%18s|%15d|%13d|\n",p.pTB,p.pNJ,p.pSI,p.pAT	);
+	printf("|%12d|%18s|%15d|%13d|\n",p->pTB, p->pNJ, p->pSI, p->pAT);
 	printf("# Resultados de las Pruebas \n"		);
 	printf("# -------------------------\n"			);
 	printf("|testId    | BEs      | BLs     | LE       | LL       | tCD       | tBD       | PF       |"		);
@@ -35,17 +35,16 @@ void print_header(FILE *csvFile, param_t *p) {
 void print_test_result(FILE *csvFile, result_t *r) {
 
 	fprintf(csvFile,"%s,%d,%d,%d,%d,%d,%d,%d\n", 
-			r.testId, r.BEs, r.BLs, r.LE, r.LL, r.tCD, r.tBD,r.PF);
+			r->testId, r->BEs, r->BLs, r->LE, r->LL, r->tCD, r->tBD, r->PF);
 	
 	printf(csvFile,"|%10s|%10d|%10d|%10d|%10d|%10d|%10d|%10d|\n", 
-				r.testId, r.BEs, r.BLs, r.LE, r.LL, r.tCD, r.tBD,r.PF);
+				r->testId, r->BEs, r->BLs, r->LE, r->LL, r->tCD, r->tBD, r->PF);
 		
 	
 	fflush(csvFile);
 }
 
-param_t verificar_filesystem(char *dev) {
-    
+param_t *verificar_filesystem(char *dev) {
 	// TODO
-	return null;
+	return NULL;
 }
