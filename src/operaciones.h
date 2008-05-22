db261c2b29c45b5a86982e5804308ecac24f5246
cdparra <cdparra@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-22 21:38:31 +0000

    Se agregaron las definiciones de las cabeceras para las mezclas de operaciones mLect2 y mDir.
    Se agregó además una Macro para realizar la medición de manera genérica.

--- src/operaciones.h
+++ src/operaciones.h
@@ -3,6 +3,12 @@
 #ifndef OPERACIONES_H_
 #define OPERACIONES_H_
 
+#define MEDICION(f) {   \
+   t1 = tiempo_milis(), \
+   f,                   \
+   t2 = tiempo_milis(), \
+   t3 = t2 -t1          \
+}
 
 //OPERACIONES
 /**
@@ -33,8 +39,12 @@ void mEscr(int cant_archivos, int tamanho, char * patron_archivos, int veces_ale
 /**
  * MLECT2
  */
-/*
- *  Leer N archivos de manera secuencial
+/* Wrapper de todas las operaciones de Lectura de este grupo
+ * 
+ */
+void mLect2(FILE output,char *path, char *patron, int cant, int cbytes);
+
+/*  Leer N archivos de manera secuencial
  * 
  *  path  : full path del directorio donde se encuentra el archivo
  *  patron: patron de nombre de los archivos.
@@ -54,7 +64,6 @@ void leer_N_aleatorio(char *path, char *patron, int cant, int cbytes);
 
 
 // OPERACIONES ESPECÍFICAS PARA ESTE BENCHMARK
-
 /* lS2000A2m: Leer 2000 archivos de 2m de forma secuencial.
  *  path  : full path del directorio donde se encuentra el archivo
  *  patron: patron de nombre de los archivos.
@@ -70,11 +79,11 @@ void leer_2000_aleatorio(char *path, char *patron);
 
 
 
-
-
 /**
  * MDIR
  */
+void mDirs(FILE fd, char *path, char *patron, int cant, int cbytes);
+
 // c100000D: Crear 100000 directorios en un directorio.
 void crear_100mil_directorios(char *path);
 
