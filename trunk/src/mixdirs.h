ec70db13130725cbe3d15138cce3024a12e3c811
cdparra <cdparra@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-22 05:37:46 +0000

    Se han implementado las operaciones relacionadas con los trabajos sobre directorios.

---
+++ trunk/src/mixdirs.h
@@ -0,0 +1,17 @@
+#ifndef MIXDIRS_H_
+#define MIXDIRS_H_
+
+/*
+ * Cabeceras y definiciones del conjunto de operaciones efectuadas sobre directorios. 
+ * 
+ */
+
+
+// c100000D: Crear 100000 directorios en un directorio.
+void crear_100mil_directorios(char *path);
+
+// b100000Dsup: Borrar el directorio que contiene los 100000 directorios.
+void borrar_100mil_directorios(char *path);
+
+
+#endif /*MIXDIRS_H_*/
