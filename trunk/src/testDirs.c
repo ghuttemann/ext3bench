4a158e75db0b33921773c10c421b25a72e000dc6
cdparra <cdparra@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-22 13:46:09 +0000

    Prueba de operaciones sobre directorios

--- trunk/src/testDirs.c
+++ trunk/src/testDirs.c
@@ -2,23 +2,36 @@
  * Pruebas básicas de las operaciones para crear y borrar directorios
  */
 
+<<<<<<< .mine
+#include "mixdirs.h" 
+#include <stdio.h>
+#include <time.h>
+=======
 #include "primitivas.h" 
+>>>>>>> .r57
 
 int main(int argc, char **argv) {
 	
 	char *dirname = "testDir";
-	char *path = "/tmp/";
+	char *path = "/tmp/ext3bench/";
+	clock_t t1,t2;
 	
 	// Prueba de crear un directorio.
 	
 	crear_directorio(path,dirname,1);
 
-	//borrar_directorio(path,dirname,1);
+	borrar_directorio(path,dirname,1);
 	
 	// Prueba de crear muchos directorios.
 	crear_directorio(path,dirname,20);
 	
-	//borrar_directorio(path,dirname,20);
+	borrar_directorio(path,dirname,20);
+	
+    printf("Creando 100.000 directorios...");	
+	crear_100mil_directorios(path);
+	
+	printf("100.000 directorios creados...");
+	borrar_100mil_directorios(path);
 	
 	
 	//return result;
