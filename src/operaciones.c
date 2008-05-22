8e70ddce3f3a3fec9db0cd1b2379a9f7e4d3a147
cdparra <cdparra@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-22 22:09:21 +0000

    Se resolvieron los conflictos existentes entre las implementaciones de las operaciones

--- src/operaciones.c
+++ src/operaciones.c
@@ -14,7 +14,7 @@
 
 #include "primitivas.h"
 #include "salida-metricas.h"
-
+#include "operaciones.h"
 
 //OPERACIONES
 
@@ -42,6 +42,7 @@ void mLect(int cant_archivos, int tamanho, char * patron_archivos, char * subdir
 	sprintf(res1.testId, "%s", "c1000A");
 	res1.BEs = (tamanho * cant_archivos ) / ((t2 - t1)/1000.0);
 	print_test_result(log, &res1);
+
 
 
 	//OPERACION  - leer los arch creados	
@@ -113,6 +114,7 @@ void mEscr(int cant_archivos, int tamanho, char * patron_archivos, int veces_ale
 
 
 
+
 	//OPERACION - RE-escritura aleatoria de los arch creados	
 	t1 = tiempo_milis();
 	for(i=0; i<cant_archivos; i++){
@@ -137,6 +139,33 @@ void mEscr(int cant_archivos, int tamanho, char * patron_archivos, int veces_ale
 /**
  * MLECT2
  */
+void mLect2(FILE output,char *path, char *patron, int cant, int cbytes) {
+
+	result_t r = {0};      	/* estructura de resultados 		*/
+	char testId[20];		/* identificador de la prueba 		*/
+	long long t1;
+	long long t2; 
+	long long t3; 	/* variables de medición de tiempo 	*/
+
+	// Primero realizamos la prueba de Lectura Secuencial
+	sprintf(testId,"lNSeq");
+	MEDICION ( leer_N_secuencial(path,patron,cant,cbytes) );
+
+	r.testId = testId;
+	r.BLs = ( cbytes / ( (double) t3) ) * 1000;
+	r.LL = ( t3 / ( (double) cant);
+	print_test_result(output,r);
+
+	sprintf(testId,"lNAleat");
+	MEDICION ( leer_N_aleatorio(path,patron,cant,cbytes) );
+
+	r.testId = testId;
+	r.BLs = ( cbytes / ( (double) t3) ) * 1000;
+	r.LL = ( t3 / ( (double) cant);
+	print_test_result(output,r);
+
+}
+
 /*
  *  Leer N archivos de manera secuencial
  * 
@@ -147,21 +176,21 @@ void mEscr(int cant_archivos, int tamanho, char * patron_archivos, int veces_ale
  */
 void leer_N_secuencial(char *path, char *patron, int cant, int cbytes) {
 	int i=0;
-    char path_buff[PATH_BUFF_SIZE + 1];
-    FILE *fd;
-    
+	char path_buff[PATH_BUFF_SIZE + 1];
+	FILE *fd;
+
 	// La longitud del patrón y la presencia de la barra al final
 	// se verifican al inicio del benchmark
-	
+
 	for (i = 0; i < cant; ++i) {
-		
+
 		sprintf(path_buff, "%s%s-%d", path, patron, i);
-		
+
 		fd = abrir_archivo(path_buff,"r");
-	    
+
 		leer_archivo(fd,path_buff,cbytes,IO_BUFF_SIZE);
-	    
-	    fclose(fd);
+
+		fclose(fd);
 	}
 }
 
@@ -174,26 +203,26 @@ void leer_N_secuencial(char *path, char *patron, int cant, int cbytes) {
  */
 void leer_N_aleatorio(char *path, char *patron, int cant, int cbytes) {
 	int i=0;
-    char path_buff[PATH_BUFF_SIZE + 1];
-    FILE *fd;
-    
+	char path_buff[PATH_BUFF_SIZE + 1];
+	FILE *fd;
+
 	// La longitud del patrón y la presencia de la barra al final
 	// se verifican al inicio del benchmark
-	
+
 	for (i = 0; i < cant; ++i) {
-		
+
 		sprintf(path_buff, "%s%s-%d", path, patron, i);
-		
+
 		fd = abrir_archivo(path_buff,"r");
-	    
+
 		// siempre se trunca, por lo que nunca se lee más de cbytes
 		int cantidad = cbytes / IO_BUFF_SIZE;
-		
+
 		io_aleatorio(fd,path_buff,cantidad,IO_BUFF_SIZE,0);
-	    
-	    fclose(fd);
+
+		fclose(fd);
 	}
-	
+
 }
 
 
@@ -217,11 +246,22 @@ void leer_2000_aleatorio(char *path, char *patron) {
 
 
 
-
-
 /**
  * MDIR
  */
+
+void mDirs(FILE fd, char *path, int cant) {
+	
+}
+
+void crear_N_directorios(char *path, int N) {
+	
+}
+
+void borrar_N_directorios(char *path, int N) {
+	
+}
+
 
 // c10000D: Crear 100000 directorios bajo un directorio.
 void crear_100mil_directorios(char *path) {
@@ -232,13 +272,13 @@ void crear_100mil_directorios(char *path) {
 	// Se crean primero 4 subdirectorios para distribuir los directorios
 	// porque cada directorio de ext3 puede tener hasta 2
 	crear_directorio(path,"testDir",4);
-	
+
 	for (var = 0; var < 4; ++var) {	
 		// En cada subdirectorio, se crean 25000 directorios. 
 		sprintf(path_buff, "%s%s-%d/", path, "testDir", var);		
 		crear_directorio(path_buff,"test",25000);
 	}
-	
+
 }
 
 // b10000Dsup: Borrar el directorio que contiene los 10000 directorios.
@@ -246,15 +286,15 @@ void borrar_100mil_directorios(char *path) {
 
 	int var = 0;
 	char path_buff[PATH_BUFF_SIZE + 1];
-	
+
 	for (var = 0; var < 4; ++var) {	
 		// En cada subdirectorio, se crean 25000 directorios. 
 		sprintf(path_buff, "%s%s-%d/", path, "testDir", var);		
 		borrar_directorio(path_buff,"test",25000);
 	}
-	
+
 	borrar_directorio(path,"testDir",4);	
-	
+
 }
 
 
@@ -268,10 +308,10 @@ void test_fragmentacion(char *path) {
 	char buffer[PATH_BUFF_SIZE + 1];
 	FILE *arch;
 	int i, j, CBYTES=600, tam;
-	
+
 	// Crear archivos vacíos.
 	crear_archivo(path, 1000, 0, 0, PATRON);
-	
+
 	// Escribir 600 bytes en cada archivo (repetir 1000 veces).
 	for (j=0; j < 1000; j++) {
 		for (i=0; i < 1000; i++) {
@@ -281,13 +321,13 @@ void test_fragmentacion(char *path) {
 			fclose(arch);
 		}
 	}
-	
+
 	// Truncar cada archivo a la mitad de su longitud
 	for (i=0; i < 1000; i++) {
 		sprintf(buffer, "%s%s-%d", path, PATRON, i);
 		arch = abrir_archivo(buffer, "w");
 		tam  = tam_archivo(arch);
-		
+
 		if (ftruncate(fileno(arch), tam/2) == -1) {
 			fprintf(stderr, 
 					"test_fragmentacion(): Error al truncar '%s'",
@@ -295,10 +335,10 @@ void test_fragmentacion(char *path) {
 			perror(NULL);
 			exit(1);
 		}
-		
+
 		fclose(arch);
 	}
-	
+
 	// Agregar 600 bytes en cada archivo (repetir 1000 veces).
 	for (j=0; j < 1000; j++) {
 		for (i=0; i < 1000; i++) {
