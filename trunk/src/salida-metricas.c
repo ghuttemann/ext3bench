272679d459c20c8c600f1e6f87fde7bd827156bc
ghuttemann <ghuttemann@87a15623-8b4d-0410-b520-5bc0c9e13cb3>
2008-05-23 21:11:49 +0000

    Se comenta la impresion de metricas en la salida estandar.

--- trunk/src/salida-metricas.c
+++ trunk/src/salida-metricas.c
@@ -10,7 +10,6 @@ void close_csvFile(FILE *fd) {
 }
 
 void print_header(FILE *csvFile, param_t *p) {
-	
 	// Imprimimo en un archivo de salida para la posterior
 	fprintf(csvFile,"# Parámetros de la Prueba\n"			);
 	fprintf(csvFile,"# -----------------------\n"			);
@@ -19,17 +18,16 @@ void print_header(FILE *csvFile, param_t *p) {
 	fprintf(csvFile,"# Resultados de las Pruebas \n"		);
 	fprintf(csvFile,"# -------------------------\n"			);
 	fprintf(csvFile,"testId,BEs,BLs,LE,LL,tCD,tBD,PF"		);
-	
-
-	// Imprimimos en salida estándar
-	printf("# Parámetros de la Prueba\n");
-	printf("# -----------------------\n");
-	printf("|Tam. Bloque | Nivel de Journal | Sync Interval | Access time |\n");
-	printf("|%12d|%18s|%15d|%13s|\n",p->pTB, p->pNJ, p->pSI, p->pAT);
-	printf("# Resultados de las Pruebas \n");
-	printf("# -------------------------\n");
-	printf("|testId    | BEs      | BLs     | LE       | LL       | tCD       | tBD       | PF-%%       | PF-min       | PF-cant       |");
 	fflush(csvFile);
+	
+//	// Imprimimos en salida estándar
+//	printf("# Parámetros de la Prueba\n");
+//	printf("# -----------------------\n");
+//	printf("|Tam. Bloque | Nivel de Journal | Sync Interval | Access time |\n");
+//	printf("|%12d|%18s|%15d|%13s|\n",p->pTB, p->pNJ, p->pSI, p->pAT);
+//	printf("# Resultados de las Pruebas \n");
+//	printf("# -------------------------\n");
+//	printf("|testId    | BEs      | BLs     | LE       | LL       | tCD       | tBD       | PF-%%       | PF-min       | PF-cant       |");
 }
 
 void print_test_result(FILE *csvFile, result_t *r) {
@@ -37,12 +35,11 @@ void print_test_result(FILE *csvFile, result_t *r) {
 			r->testId, r->BEs, r->BLs, r->LE, r->LL, r->tCD, r->tBD, 
 			r->PF.porcentaje, r->PF.minimo, r->PF.cant_arch);
 	
-	printf("|%10s|%10f|%10f|%10f|%10f|%10f|%10f|%10f|%10f|%10d|\n", 
-			r->testId, r->BEs, r->BLs, r->LE, r->LL, r->tCD, r->tBD, 
-			r->PF.porcentaje, r->PF.minimo, r->PF.cant_arch);
-		
-	
 	fflush(csvFile);
+	
+//	printf("|%10s|%10f|%10f|%10f|%10f|%10f|%10f|%10f|%10f|%10d|\n", 
+//			r->testId, r->BEs, r->BLs, r->LE, r->LL, r->tCD, r->tBD, 
+//			r->PF.porcentaje, r->PF.minimo, r->PF.cant_arch);
 }
 
 param_t *verificar_filesystem(char *dev) {
